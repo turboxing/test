397744c8d055814cb3fa349052b4c5e8a35e3e51
红色小星 <695906942@qq.com>
2021-03-18 23:20:23 +0800

    Update createtag.sh
    update echo exec result

--- createtag.sh
+++ createtag.sh
@@ -1,13 +1,38 @@
 #!/bin/bash
 echo 'please tag name'：
 read tagName
-echo 'please tag desc'：
-read tagdesc
-echo "your enter tag: $tagName"
-echo "your enter tagdesc: $tagdesc"
-git tag -a $tagName -m $tagdesc
-git push origin $tagName
-echo "create $tagName success!"
-echo "current tag list:" 
-git tag 
-echo "done()"
+if [ ! $tagName ]; 
+then 
+    echo 'fail:tagName is not empty'
+else
+    echo 'please tag desc'：
+    read tagdesc
+    if [ ! $tagdesc ]; 
+    then
+        echo 'fail:tagdesc is not empty'
+    else 
+    echo "your enter tag: $tagName"
+    echo "your enter tagdesc: $tagdesc"
+    git tag -a $tagName -m $tagdesc
+    if [ $? -ne 0 ]; 
+    then
+    echo "git create tag failed!"
+    else
+    echo -e "\033[32;40mgit create tag success！\033[0m"
+    git push origin $tagName
+    if [ $? -ne 0 ]; 
+    then 
+    echo "git push origin $tagName failed!"
+    else 
+    # echo "git push origin $tagName success!"
+    echo -e "\033[32;40mgit push origin $tagName success!\033[0m"
+    echo "current all tag :" 
+    git tag 
+    fi
+    fi
+    
+    fi
+
+    echo "done"
+fi
+
