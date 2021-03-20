6c9cd7e6f9e8fb00fb9f4e4c442ea38fb6f56fc9
suncx <1965259633@qq.com>
2021-03-20 17:46:59 +0800

    update shell

--- createtag.sh
+++ createtag.sh
@@ -1,79 +1,75 @@
 #!/bin/bash
-# echo 'please tag name'：
-# read tagName
-# if [ ! $tagName ]; 
-# then 
-#     echo 'fail:tagName is not empty'
-# else
-#     echo 'please tag desc'：
-#     read tagdesc
-#     if [ ! $tagdesc ]; 
-#     then
-#         echo 'fail:tagdesc is not empty'
-#     else 
-#     echo "your enter tag: $tagName"
-#     echo "your enter tagdesc: $tagdesc"
-#     git tag -a $tagName -m $tagdesc
-#     if [ $? -ne 0 ]; 
-#     then
-#     echo "git create tag failed!"
-#     else
-#     echo -e "\033[32;40mgit create tag success！\033[0m"
-#     git push origin $tagName
-#     if [ $? -ne 0 ]; 
-#     then 
-#     echo "git push origin $tagName failed!"
-#     else 
-#     # echo "git push origin $tagName success!"
-#     echo -e "\033[32;40mgit push origin $tagName success!\033[0m"
-#     echo "current all tag :" 
-#     git tag 
-#     fi
-#     fi
-    
-#     fi
-
-#     echo "done"
-# fi
-
-
+# 1. 输入tag名字和描述
+# 2. 替换podSpoec中的版本号
+# 3. 打tag
+# 4. push tag 到远端
 #  遍历文件内容，找到s.verison 开始的行，用新的内容替换；exit 是关闭程序
 
-function updatePodVersion(){
-    while read LINE
-do
-	#记录行数
-	let count++
-	#打印行号及其内容
-	echo "$count - $LINE"
-
-    result=$(echo $LINE | grep "s.version") 
-    echo $result
-    if [ "$result" != "" ]
-    then 
-        echo "包涵"
-        a='a'
-        d='d'
- 	sed -i "_bak" '1a\
-         drink tea' Podfile
-        sed "$(LINE)d" Podfile
-    exit
-    else
-        echo "no"
-    fi
-done < Podfile
-
-#     podFileContent=$(cat Podfile)
-#     echo $podFileContent
-#     strB="2.1.4"
-
-# result=$(echo $podFileContent | grep "${strB}") 
-
-# if [[ "$result" != "" ]]
-# then
-#     echo "包含"
-# else
-#     echo "不包含"
-# fi
+function updatePodSpecVersion(){
+    temTagName=$1
+# 匹配+s.version开头的内容然后替换
+ sed -i '' "/^[[:space:]]s.version/c\
+\    \s.version = '$temTagName'
+" Podfile
+if [ $? -ne 0 ];
+then
+return -1;
+else
+return 0;
+fi
 }
-updatePodVersion
+
+echo 'please tag name'：
+read tagName
+if [ ! $tagName ];
+then
+    echo 'fail:tagName is not empty'
+    exit
+fi
+
+
+echo 'please tag desc'：
+read tagdesc
+if [ ! $tagdesc ];
+then
+    echo 'fail:tagdesc is not empty'
+    exit
+fi
+ 
+    echo "your enter tag: $tagName"
+    echo "your enter tagdesc: $tagdesc"
+    echo "start update PodSpec Version"
+
+    updatePodSpecVersion $tagName
+    if [ $? -ne 0 ];
+    then
+    echo " update PodSpec Version fail"
+    exit
+    fi
+
+    echo -e "\033[32;40mupdate PodSpec Version success!\033[0m"
+
+    echo "start create tag"
+    git tag -a $tagName -m $tagdesc
+    if [ $? -ne 0 ];
+    then
+    echo "git create tag failed!"
+    exit
+    fi
+
+    echo -e "\033[32;40mgit create tag success！\033[0m"
+    echo "start push tag to origin"
+    git push origin $tagName
+    if [ $? -ne 0 ];
+    then
+    echo "git push origin $tagName failed!"
+    exit
+    fi
+
+    echo -e "\033[32;40mgit push $tagName to origin  success!\033[0m"
+
+    echo "current all tag :"
+    git tag
+
+    echo "done"
+
